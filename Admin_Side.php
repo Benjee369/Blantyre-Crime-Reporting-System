bc7caf6f174fb1e13de9ee68d4bc27da401991d1
Benjee369 <benjaminphiri369@gmail.com>
2024-03-13 11:28:59 +0200

    Changed the nav bar to only display logout to the admins and the officers once they are logged in and removed the hospital icons from the dashboard page

--- Admin_Side.php
+++ Admin_Side.php
@@ -35,8 +35,7 @@ session_start();
             <div class="content">
                 <div class="row">
                 <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
-                    <div class="dash-widget">
-                        <span class="dash-widget-bg1"><i class="fa fa-stethoscope" aria-hidden="true"></i></span>
+                    <div class="dash-widget">   
                         <div class="dash-widget-info text-right">
                         <?php
                             require_once 'DatabaseConn.php';
@@ -66,7 +65,6 @@ session_start();
 
                     <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
                         <div class="dash-widget">
-                            <span class="dash-widget-bg2"><i class="fa fa-user-o"></i></span>
                             <div class="dash-widget-info text-right">
                             <?php
                                 require_once 'DatabaseConn.php';
@@ -92,7 +90,6 @@ session_start();
                     </div>
                     <div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
                         <div class="dash-widget">
-                            <span class="dash-widget-bg3"><i class="fa fa-user-md" aria-hidden="true"></i></span>
                             <div class="dash-widget-info text-right">
                                 <h3>72</h3>
                                 <span class="widget-title3">Attend <i class="fa fa-check" aria-hidden="true"></i></span>
--- nvbr.php
+++ nvbr.php
@@ -36,38 +36,56 @@
 
             <div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
               <ul class="navbar-nav  ">
-                <li class="nav-item active">
-                  <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="about.php"> About</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="officers.php">Officers</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="contact.php">Contact us</a>
-                </li>
-
-                <?php
+              <?php
+                // Check if a general user is logged in
                 if (isset($_SESSION['user_id'])) {
-                  echo '<li class="nav-item">
-                          <a class="nav-link" href="Logout.php">Logout</a>
-                        </li>';
+                    // If a user is logged in, display the logout button and all other navigation items
+                    echo '<li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="about.php">About</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="officers.php">Officers</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact us</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="Logout.php">Logout</a>
+                          </li>';
+                } elseif (isset($_SESSION['admin_id']) || isset($_SESSION['officer_id'])) {
+                    // If an admin or officer is logged in, only display the logout button
+                    echo '<li class="nav-item">
+                            <a class="nav-link" href="Logout.php">Logout</a>
+                          </li>';
                 } else {
-                  echo '<li class="nav-item">
-                          <a class="nav-link" href="Login.php">Login</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="Create_Account.php">Create Account</a>
-                        </li>';
+                    // If no user, admin, or officer is logged in, display login and create account options
+                    echo '<li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="about.php">About</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="officers.php">Officers</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact us</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="Login.php">Login</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="Create_Account.php">Create Account</a>
+                          </li>';
                 }
                 ?>
-                
+             
                 <li class="nav-item">
                   <a class="nav-link" href="Admin_Side.php">Admin Side</a>
-                </li>
-                
+                </li>                
               </ul>
             </div>
           </nav>
